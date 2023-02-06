e1d09f4482b7eb6548e005d173ace01e496a61a7
YesNoah <98677277+YesNoah@users.noreply.github.com>
2023-02-06 14:06:00 -0500

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 import click
 import logging
 import tensorflow as tf
@@ -6,10 +6,7 @@ from pathlib import Path
 import os
 from dotenv import find_dotenv, load_dotenv
 
-os.getcwd()
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -25,35 +22,24 @@ def main(input_filepath, output_filepath):
                                                             shuffle=True,
                                                             batch_size=BATCH_SIZE,
                                                             image_size=IMG_SIZE)
-    validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
+    test_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
                                                                  shuffle=True,
                                                                  batch_size=BATCH_SIZE,
                                                                  image_size=IMG_SIZE)
     class_names = train_dataset.class_names
-    val_batches = tf.data.experimental.cardinality(validation_dataset)
-    test_dataset = validation_dataset.take(val_batches // 5)
-    validation_dataset = validation_dataset.skip(val_batches // 5)
+    val_batches = tf.data.experimental.cardinality(train_dataset)
+
+    validation_dataset = train_dataset.take(val_batches // 5)
+
+    train_dataset = train_dataset.skip(val_batches // 5)
+
     AUTOTUNE = tf.data.AUTOTUNE
 
     train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
     validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
     test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
 
-    return (train_dataset, validation_dataset, test_dataset)
+    return (train_dataset, validation_dataset, test_dataset, class_names)
 
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
